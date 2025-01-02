dac2526198afa6d5da0e91efaf0860277711f99f
Funnyklown <amorroni@protonmail.com>
2025-01-02 12:50:00 +0100

    use of fork of axum_keycloak to patch soundness issues, begin protecting routes

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,53 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use axum_keycloak_auth::{instance::{KeycloakAuthInstance, KeycloakConfig}, layer::KeycloakAuthLayer, PassthroughMode};
 use charizhard_ota::route::{root, specific_firmware};
+use reqwest::Url;
 use route::{delete_firmware, latest_firmware, post_firmware};
 use std::result::Result::Ok;
 mod route;
+
+pub fn public_router() -> Router {
+    Router::new()
+        .route("/", get(root))
+        .route("/latest", get(latest_firmware))
+        .route("/firmware/:file_name", get(specific_firmware))
+}
+
+pub fn protected_router(instance: KeycloakAuthInstance) -> Router {
+    Router::new().route(
+        "/firmware/:file_name",
+        post(post_firmware).delete(delete_firmware),
+    )
+    .layer(
+        KeycloakAuthLayer::<String>::builder()
+            .instance(instance)
+            .passthrough_mode(PassthroughMode::Block)
+            .persist_raw_claims(false)
+            .expected_audiences(vec![String::from("account")])
+            .required_roles(vec![String::from("administrator")])
+            .build(),
+   )
+
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
-    // build our application with a route
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/latest", get(latest_firmware))
-        .route(
-            "/firmware/:file_name",
-            get(specific_firmware)
-                .post(post_firmware)
-                .delete(delete_firmware),
-        );
+    let keycloak_auth_instance = KeycloakAuthInstance::new(
+        KeycloakConfig::builder()
+            .server(Url::parse("http://127.0.0.1:8180").unwrap())
+            .realm(String::from("charizhard-ota"))
+            .build(),
+    );
+
+    let router = public_router().merge(protected_router(keycloak_auth_instance));
+
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await?;
-    axum::serve(listener, app).await?;
+    axum::serve(listener, router.into_make_service()).await?;
     Ok(())
 }
